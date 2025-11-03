ccfbf1bae69e09016555cc48dcc7702852972b82
Trevor Katemba <drxpnotlox@gmail.com>
2025-11-03 18:00:28 -0500

    Added BPE tokenizer

--- tkatemb/tokenization/tokenize.py
+++ tkatemb/tokenization/tokenize.py
@@ -1,6 +1,7 @@
 import re
+import tiktoken
 
-class SimpleTokenizerV1:
+class SimpleTokenizerV2:
     def __init__(self, vocab):
         self.str_to_int = vocab
         self.int_to_str = {i:s for s,i in vocab.items()}
@@ -11,12 +12,14 @@ class SimpleTokenizerV1:
         preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
         # Loop through every token and if it's not whitespace (.strip returns null if it's empty) then return the stripped version of the token
         preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        preprocessed = [item if item in self.str_to_int
+                        else f"<|unk|>" for item in preprocessed]
         ids = [self.str_to_int[s] for s in preprocessed]
         return ids
     
     def decode(self, ids):
         text = " ".join([self.int_to_str[i] for i in ids])
-        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        text = re.sub(r'\s+([,.:;?!"()\'])', r'\1', text)
         return text
 
 
@@ -32,12 +35,12 @@ print(f"Total number of tokens: {len(preprocessed)}")
 print(preprocessed[:30])
 
 all_words = sorted(set(preprocessed))
+all_words.extend(["<|endoftext|>", "<|unk|>"])
 vocab_size = len(all_words)
 print(vocab_size)
 
 vocab = {token:integer for integer,token in enumerate(all_words)}
-
-tokenizer = SimpleTokenizerV1(vocab=vocab)
+tokenizer = SimpleTokenizerV2(vocab=vocab)
 text = "\"It's the last he painted, you know,\" Mrs. Gisburn said with pardonable pride."
 
 ids = tokenizer.encode(text=text)
@@ -46,4 +49,16 @@ print(ids)
 words = tokenizer.decode(ids)
 print(words)
 
- 
+text1 = "Hello, do you like tea?"
+text2 = "In the sunlit terraces of the palace."
+text = " <|endoftext|> ".join((text1, text2))
+print(text)
+
+
+print(tokenizer.decode(tokenizer.encode(text)))
+
+# Byte-pair encoding:P
+
+tokenizer = tiktoken.get_encoding("gpt2")
+integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+print(integers)
