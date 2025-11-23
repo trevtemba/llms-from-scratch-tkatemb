1a26f0abb74eac0d753f91376a92dd5c06af4867
Trevor Katemba <drxpnotlox@gmail.com>
2025-11-23 17:24:08 -0500

    Added training/validation evaluation, using cross entropy

--- tkatemb/main.py
+++ tkatemb/main.py
@@ -2,10 +2,12 @@ import tiktoken
 import torch
 import GPT
 import torch.nn as nn
+from train_eval import train_util as util
+from train_eval import dataloader
 
 GPT_CONFIG_124M = {
     "vocab_size": 50257, #vocab of 50257 words used by BPE tokenizer
-    "context_length": 1024, # max num of input tokens model can handle via the positional embeddings
+    "context_length": 256, # max num of input tokens model can handle via the positional embeddings
     "emb_dim": 768,# each token will be transformed into a 768 dim vector,
     "n_heads": 12, # amount of attention heads in mha
     "n_layers": 12, # number of transformer blocks in the model
@@ -61,11 +63,7 @@ if __name__ == "__main__":
 
     torch.manual_seed(123)
     model = GPT.GPTModel(GPT_CONFIG_124M)
-    out = model(batch)
-
-    print("Input shape:", batch)
-    print("Output shape:", out.shape)
-    print(out)
+    model.eval()
 
     # # compute parameter count
     # total_params = sum(p.numel() for p in model.parameters())
@@ -74,16 +72,110 @@ if __name__ == "__main__":
     # total_mem_mb = total_mem_bytes / (1024 * 1024)
     # print(f"Total size of model: {total_mem_mb:.2f} MB")
 
-    start_context = "Hello, I am"
-    encoded = tokenizer.encode(start_context)
-    print("encoded:", encoded)
-    encoded_tensor = torch.tensor(encoded).unsqueeze(0)
-    print("encoded_tensor shape:", encoded_tensor.shape)
+    # start_context = "Every effort moves you"
+    
+    # token_ids = GPT.generate_text_simple(
+    #     model=model,
+    #     idx=util.text_to_token_ids(start_context, tokenizer),
+    #     max_new_tokens=10,
+    #     context_size=GPT_CONFIG_124M["context_length"],
+    # )
+    # print("Output text:\n", util.token_ids_to_text(token_ids, tokenizer))
+    
+    # inputs = torch.tensor([[16833, 3626, 6100], # "every effort moves",
+    #                        [40, 1107, 588]]) # "I really like"
+    
+    # targets = torch.tensor([[3626, 6100, 345], # "effort moves you"
+    #                         [1107, 588, 11311]]) # "really like chocolate"
+    
+    # with torch.no_grad():
+    #     logits = model(inputs)
 
-    model.eval()
-    out = GPT.generate_text_simple(model=model, idx=encoded_tensor, max_new_tokens=6, context_size=GPT_CONFIG_124M["context_length"])
-    print("Output:", out)
-    print("Output length:", len(out[0]))
+    # probas = torch.softmax(logits, dim=-1)
+    # print(probas.shape)
 
-    text = tokenizer.decode(out.squeeze(0).tolist())
-    print(text)
+    # token_ids = torch.argmax(probas, dim=-1, keepdim=True)
+    # print("Token IDs:\n", token_ids)
+
+    # print(f"Targets batch 1: {util.token_ids_to_text(targets[0], tokenizer)}")
+    # print(f"Outputs batch 1: {util.token_ids_to_text(token_ids[0].flatten(), tokenizer)}")
+
+    # # Manual cross entropy
+    # text_idx = 0
+    # target_probas_1 = probas[text_idx, [0, 1, 2], targets[text_idx]]
+    # print("Text 1:", target_probas_1)
+
+    # text_idx = 1
+    # target_probas_2 = probas[text_idx, [0, 1, 2], targets[text_idx]]
+    # print("Text 2:", target_probas_2)
+
+    # log_probas = torch.log(torch.cat((target_probas_1, target_probas_2)))
+    # print(log_probas)
+    # avg_log_probas = torch.mean(log_probas)
+    # print(avg_log_probas)
+
+    # neg_avg_log_probas = avg_log_probas * -1
+    # print(neg_avg_log_probas)
+
+    # # Industry
+    # logits_flat = logits.flatten(0, 1)
+    # targets_flat = targets.flatten()
+    # print("Flattened logits:", logits_flat.shape)
+    # print("Flattened targets:", targets_flat.shape)
+
+    # loss = torch.nn.functional.cross_entropy(logits_flat, targets_flat)
+    # print(loss)
+
+    file_path = "tkatemb/tokenization/the_verdict.txt"
+    with open(file_path, "r", encoding="utf-8") as file:
+        text_data = file.read()
+    total_characters = len(text_data)
+    total_tokens = len(tokenizer.encode(text_data))
+    print("Characters:", total_characters)
+    print("Tokens:", total_tokens)
+
+    train_ratio = 0.9
+    split_idx = int(train_ratio * len(text_data))
+    train_data = text_data[:split_idx]
+    val_data = text_data[split_idx:]
+
+    train_loader = dataloader.create_dataloader_v1(
+        txt=train_data,
+        batch_size=2,
+        max_length=GPT_CONFIG_124M["context_length"],
+        stride=GPT_CONFIG_124M["context_length"],
+        drop_last=True,
+        shuffle=True,
+        num_workers=0,
+    )
+    
+    
+    val_loader = dataloader.create_dataloader_v1(
+        txt=val_data,
+        batch_size=2,
+        max_length=GPT_CONFIG_124M["context_length"],
+        stride=GPT_CONFIG_124M["context_length"],
+        drop_last=False,
+        shuffle=False,
+        num_workers=0,
+    )
+
+    print("Train loader:")
+    for x, y in train_loader:
+        print(x.shape, y.shape)
+        
+    print("\nValidation loader:")
+    for x, y in val_loader:
+        print(x.shape, y.shape)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    with torch.no_grad():
+        train_loss = util.calc_loss_loader(train_loader, model, device)
+        val_loss = util.calc_loss_loader(val_loader, model, device)
+
+    print("Training loss:", train_loss)
+    print("Validation loss:", val_loss)
+    
+
+
---
+++ tkatemb/train_eval/train_util.py
@@ -0,0 +1,43 @@
+import tiktoken
+import torch
+
+def text_to_token_ids(text, tokenizer):
+    encoded = tokenizer.encode(text, allowed_special={'<|endoftext|>'})
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+    return encoded_tensor
+
+def token_ids_to_text(token_ids, tokenizer):
+    flat = token_ids.squeeze(0)
+    return tokenizer.decode(flat.tolist())
+
+# Computes the loss for a single batch
+def calc_loss_batch(input_batch, target_batch, model, device):
+    input_batch = input_batch.to(device)
+    target_batch = target_batch.to(device)
+    logits = model(input_batch)
+
+    loss = torch.nn.functional.cross_entropy(
+        logits.flatten(0,1), target_batch.flatten()
+    )
+    return loss
+
+# Iterates over all batches in a given data loader and returns the average loss of the total number of batches
+def calc_loss_loader(data_loader, model, device, num_batches=None):
+    total_loss = 0.
+    if len(data_loader) == 0:
+        return float("nan")
+    elif num_batches is None:
+        num_batches = len(data_loader)
+    else:
+        num_batches = min(num_batches, len(data_loader))
+    for i, (input_batch, target_batch) in enumerate(data_loader):
+        if i < num_batches:
+            loss = calc_loss_batch(
+                input_batch, target_batch, model, device
+            )
+            total_loss += loss.item()
+        else:
+            break
+    
+    return total_loss/num_batches
+
